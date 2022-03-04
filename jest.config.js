07fbe2d9186f087058b2e4ca133031ede63fc799
Denis Ustiuzhanin <superden1320@gmail.com>
2022-03-04 18:59:22 +0100

    Add unit tests

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testPathIgnorePatterns: [
+    "<rootDir>/.next/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/__test__/e2e/",
+  ],
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+    "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js",
+  },
+  transform: {
+    "^.+\\.(ts|tsx|js|jsx)$": "babel-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "json"],
+};
